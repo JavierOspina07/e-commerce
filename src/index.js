d38433f1e2eb7211a2eea83dddf7f84ebdaa32a8
Javier Ospina <jaheosig10@gmail.com>
2023-06-09 15:18:49 -0500

    "agrago boton darkmode"

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ async function fetchData() {
   
       const addToCartBtn = productElement.querySelector('.add__to__cart__btn');
       addToCartBtn.addEventListener('click', () => {
-        addToCart(product);
+        addToCartBtn(product);
       });
     });
   } catch (error) {
@@ -64,7 +64,36 @@ async function fetchData() {
 
 fetchData();
 
-          /* ====================== API CON THEN===================== */
+/* ==============  BONTON DARKMODE  ============= */
+
+
+const btnSwitch = document.querySelector('#switch')
+
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  btnSwitch.classList.toggle('active')
+
+  /* guardar el modo dark */
+  if(document.body.classList.contains('dark')){
+    localStorage.setItem('dark-mode' , 'true');
+  }else{
+    localStorage.setItem('dark-mode' , 'false')
+  }
+})
+
+/* Obtenermos el modo actual */
+if(localStorage.getItem('dark-mode') === 'true'){
+  document.body.classList.add('dark');
+  btnSwitch.classList.add('active')
+}else{
+  document.body.classList.remove('dark');
+  btnSwitch.classList.remove('active');
+}
+
+
+
+
+/* ====================== API CON THEN===================== */
           // fetch('https://ecommercebackend.fundamentos-29.repl.co')
           //   .then(res => res.json())
           //   .then(data => {
@@ -102,3 +131,5 @@ fetchData();
           //     });
           //   })
           //   .catch(error => console.error(error));
+
+          
