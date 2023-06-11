629d87887eb390b30e4d9365109a69bac0cc6be8
Javier Ospina <jaheosig10@gmail.com>
2023-06-11 11:10:05 -0500

    "cambios principales del navbar"

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ const productOverlay = document.getElementById('product__overlay');
 const closeBtn = document.getElementById('close__btn');
 const productName = document.getElementById('product__name');
 const productPrice = document.getElementById('product__price');
-
+const productImage = document.getElementById('product__image');
 
 /* ====================== Detalles del producto ===================== */
 
 function showProductDetails(product) {
   productName.textContent = product.name;
   productPrice.textContent = `$${product.price}`;
+  // productImage.setAttribute= product.image;
   productOverlay.style.display = 'flex';
 }
 
@@ -17,15 +18,13 @@ closeBtn.addEventListener('click', () => {
   productOverlay.style.display = 'none';
 });
 
-
-
 /* API CON ASYNC */
 
 async function fetchData() {
   try {
     const response = await fetch('https://ecommercebackend.fundamentos-29.repl.co');
     const data = await response.json();
-    
+
     data.forEach(product => {
       const productElement = document.createElement('div');
       productElement.classList.add('product__card');
@@ -33,12 +32,12 @@ async function fetchData() {
       <div class="image__container">
           <img src="${product.image}" alt="${product.name}">
           <button class="add__to__cart__btn"><i class='bx bx-shopping-bag'></i></button>
-        </div>
+      </div>
         <div class="product__info">
           <div class="name">${product.name}</div>
         </div>
         <div class="price">$${product.price}</div>
-        <p>${product.description}</p>
+          <p>${product.description}</p>
         <div class="additional__info">
           <p class="category">Categoría: ${product.category}</p>
           <p class="quantity">Cantidad: ${product.quantity}</p>
@@ -46,17 +45,17 @@ async function fetchData() {
         <button class="details__btn">Ver detalles</button>
       `;
       productContainer.appendChild(productElement);
-  
+
       const detailsBtn = productElement.querySelector('.details__btn');
       detailsBtn.addEventListener('click', () => {
         showProductDetails(product);
       });
-  
+
       const addToCartBtn = productElement.querySelector('.add__to__cart__btn');
       addToCartBtn.addEventListener('click', () => {
         addToCartBtn(product);
       });
-      
+
     });
   } catch (error) {
     console.error(error);
@@ -67,30 +66,38 @@ fetchData();
 
 /* ==============  BONTON DARKMODE  ============= */
 
-
 const btnSwitch = document.querySelector('#switch')
 
 btnSwitch.addEventListener('click', () => {
   document.body.classList.toggle('dark');
-  btnSwitch.classList.toggle('active')
+  btnSwitch.classList.toggle('active');
 
   /* guardar el modo dark */
-  if(document.body.classList.contains('dark')){
-    localStorage.setItem('dark-mode' , 'true');
-  }else{
-    localStorage.setItem('dark-mode' , 'false')
+  if (document.body.classList.contains('dark')) {
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
   }
 })
 
-/* Obtenermos el modo actual */
-if(localStorage.getItem('dark-mode') === 'true'){
+/* Obtener el modo actual */
+if (localStorage.getItem('dark-mode') === 'true') {
   document.body.classList.add('dark');
-  btnSwitch.classList.add('active')
-}else{
+  btnSwitch.classList.add('active');
+} else {
   document.body.classList.remove('dark');
-  document.header.classList.remove('dark');
   btnSwitch.classList.remove('active');
 }
+
+/* ==========  animacion del navbar  ==========  */
+
+window.addEventListener('scroll', function(){
+  var header = document.querySelector('header');
+  header.classList.toggle('abajo',window.scrollY > 0);
+})
+
+
+
 
 
 
